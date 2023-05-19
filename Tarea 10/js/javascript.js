60c4ed0bf63698e2feaca18736cec038d3d292b7
thiagodipizio <130709425+thiagodipizio@users.noreply.github.com>
2023-05-19 12:11:07 -0300

    Add files via upload

---
+++ Tarea 10/js/javascript.js
@@ -0,0 +1,9 @@
+var nombre = "thiago";
+var apellido = "dipizio";
+console.log(nombre + apellido);
+var numero1 = 100;
+var numero2 = 300;
+var suma = (numero1 + numero2);
+console.log(suma);
+var numero3 = (suma + 500);
+console.log(numero3);
